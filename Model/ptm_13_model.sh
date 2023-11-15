21a5f590efb8a898726d34f9b09446d90376b082
Astrid Brix Saksager <astridsaksager@gmail.com>
2023-11-15 13:20:31 +0100

    queque and run ptm

--- Model/ptm_13_model.sh
+++ Model/ptm_13_model.sh
@@ -8,6 +8,8 @@
 # Download data into /dtu/blackhole/17/126583
 #workshop download ptm
 
+echo 'this works'
+
 # Train a model 
 workshop train  train dataset=ptm_13 encoder=schnet task=multiclass_node_classification trainer=gpu 
 #env.paths.data
