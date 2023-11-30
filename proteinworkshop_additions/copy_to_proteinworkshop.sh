8d02e143d8779e75c1d580018b24bcdc2112fcda
Astrid Brix Saksager <astridsaksager@gmail.com>
2023-11-30 13:06:23 +0100

    add and modified config files on multi encoding

--- proteinworkshop_additions/copy_to_proteinworkshop.sh
+++ proteinworkshop_additions/copy_to_proteinworkshop.sh
@@ -9,3 +9,7 @@ cd /zhome/ce/4/118546/deeplearning
 cp ./DL_project/proteinworkshop_additions/our.py ./env/lib/python3.10/site-packages/proteinworkshop/datasets/our.py
 cp ./DL_project/proteinworkshop_additions/our.yaml ./env/lib/python3.10/site-packages/proteinworkshop/config/dataset/our.yaml
 cp ./DL_project/proteinworkshop_additions/load.yaml ./env/lib/python3.10/site-packages/proteinworkshop/config/dataset/load.yaml
+cp ./DL_project/proteinworkshop_additions/base.py   ./env/lib/python3.10/site-packages/proteinworkshop/datasets/base.py
+cp ./DL_project/proteinworkshop_additions/multihot_label_encoding.py ./env/lib/python3.10/site-packages/proteinworkshop/tasks/multihot_label_encoding.py
+cp ./DL_project/proteinworkshop_additions/multilabel_node_classification.yaml ./env/lib/python3.10/site-packages/proteinworkshop/config/task/multilabel_node_classification.yaml
+cp ./DL_project/proteinworkshop_additions/multihot_label_encoding.yaml ./env/lib/python3.10/site-packages/proteinworkshop/config/transforms/multihot_label_encoding.yaml
